9ae515cf0d7be878b7c3bf540f3763c29db7bc92
felipenino13 <cristianfelipenino@gmail.com>
2023-09-10 20:01:00 -0500

    ajuste en mobile para que salga el pop up se debe dejar el acordeon con la descripcion para que se vea en mobile

--- javascript/arcgis.js
+++ javascript/arcgis.js
@@ -109,9 +109,11 @@ require([
     map.add(layer);
 
     // Define a pop-up for Trailheads
+    let x = "<img src='https://services6.arcgis.com/4bqDruSLRri6LXWK/arcgis/rest/services/municipios/FeatureServer/0/{OBJECTID}/attachments/{OBJECTID}'><br><b>Municipio:</b> {Municipio}<br> <b>Departamento:</b> {departamento}<br><hr><b>UFH encontradas:</b> {ufhEncontradas}<br><b>UFH líder</b> {ufhLider}<br><hr><b>Líneas productivas:</b> {lineasProductivas}<br> <b>Líneas agrícolas:</b> {lineasAgricolas}<br><b>Líneas pecuarias:</b> {lineasPecuarias}<br><br>";
+
     const popupTrailheads = {
       "title": "Descripción",
-      "content": "<img src='https://services6.arcgis.com/4bqDruSLRri6LXWK/arcgis/rest/services/municipios/FeatureServer/0/{OBJECTID}/attachments/{OBJECTID}'><br><b>Municipio:</b> {Municipio}<br> <b>Departamento:</b> {departamento}<br><hr><b>UFH encontradas:</b> {ufhEncontradas}<br><b>UFH líder</b> {ufhLider}<br><hr><b>Líneas productivas:</b> {lineasProductivas}<br> <b>Líneas agrícolas:</b> {lineasAgricolas}<br><b>Líneas pecuarias:</b> {lineasPecuarias}<br><br>",
+      "content": x
 
     }
 
